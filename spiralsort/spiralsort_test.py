e3d1516d274874e057dd86b565d3bb6ab41fd8d5
Thanasis Mattas <atmattas@physics.auth.gr>
2020-10-10 23:03:01 +0300

    test: organize tests into classes

--- spiralsort/spiralsort_test.py
+++ spiralsort/spiralsort_test.py
@@ -23,100 +23,104 @@ import pytest
 from spiralsort import core, io, utils
 
 
-# core.py tests
-def test_counterclockwise_filter():
-    prev_node_mock = pd.Series({'x': 2, 'y': 1})
-    nodes_mock = pd.DataFrame(
-        {
-            'x': [-2, -2, -2, 1, 1, 2, 2, 4, 4, 5, 40, 40],
-            'y': [1, -0.5, -1.5, -0.5, -1, 2, -1, 3, 1, 5, 21, 19]
-        }
-    )
-    index_expected = pd.Index([0, 1, 5, 7, 9, 10])
-    index_filtered = core._counterclockwise_filter(nodes_mock, prev_node_mock)
-    assert_index_equal(index_expected, index_filtered)
+class TestCore:
+    """core.py tests"""
+
+    def test_counterclockwise_filter(self):
+        prev_node_mock = pd.Series({'x': 2, 'y': 1})
+        nodes_mock = pd.DataFrame(
+            {
+                'x': [-2, -2, -2, 1, 1, 2, 2, 4, 4, 5, 40, 40],
+                'y': [1, -0.5, -1.5, -0.5, -1, 2, -1, 3, 1, 5, 21, 19]
+            }
+        )
+        index_expected = pd.Index([0, 1, 5, 7, 9, 10])
+        index_filtered = core._counterclockwise_filter(nodes_mock,
+                                                       prev_node_mock)
+        assert_index_equal(index_expected, index_filtered)
+
+    def test_spiralsort(self):
+        spiralsorted_expected = pd.DataFrame(
+            {
+                #     0, 1, 2, 3, 4, 5,  6,  7,  8,  9, 10, 11, 12, 13, 14,15,16,17,18,19  20, 21, 22,23,24,25
+                'x': [0, 1, 2, 2, 1, 0, -1, -2, -2, -2, -1,  0,  1,  2,  3, 3, 3, 2, 1, 0, -2, -2,  3, 3, 2, 3],  # noqa: E241
+                'y': [0, 0, 0, 1, 2, 2,  2,  1,  0, -1, -2, -2, -2, -2, -1, 0, 1, 2, 3, 3,  2, -2, -2, 2, 3, 3],  # noqa: E241
+                'z': [0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0, 0,  0,  0,  0, 0, 0, 0]   # noqa: E241
+            }
+        )
+        max_digits = len(str(spiralsorted_expected.index.max()))
+        node_id = ["N_" + (max_digits - len(str(idx))) * '0' + str(idx)
+                   for idx in spiralsorted_expected.index]
+        spiralsorted_expected.insert(0, "node_id", node_id)
+        spiralsorted_expected = spiralsorted_expected.astype(
+            {'x': "float", 'y': "float", 'z': "float"}
+        )
+
+        # format
+        # ---------------------------
+        #    node_id    x    y    z
+        # 0     N_00  0.0  0.0  0.0
+        # 1     N_01  1.0  0.0  0.0
+        # ...
+
+        #             19  18  24   25
+        #
+        #      20  6   5   4  17   23
+        #
+        #       7              3   16
+        #
+        #       8      0   1   2   15
+        #
+        #       9                  14
+        #
+        #      21 10  11  12  13   22
+
+        nodes_mock = spiralsorted_expected.copy()
+        # shuffle the DataFrame
+        nodes_mock = nodes_mock.sample(frac=1, random_state=9) \
+                               .reset_index(drop=True)
+        spiralsorted = core.spiralsort(nodes_mock,
+                                       spiralsorted_expected.loc[0, "node_id"])
+        assert_frame_equal(spiralsorted_expected,
+                           spiralsorted.iloc[:, [0, 1, 2, 3]])
 
 
-def test_spiralsort():
-    spiralsorted_expected = pd.DataFrame(
-        {
-            #     0, 1, 2, 3, 4, 5,  6,  7,  8,  9, 10, 11, 12, 13, 14,15,16,17,18,19  20, 21, 22,23,24,25
-            'x': [0, 1, 2, 2, 1, 0, -1, -2, -2, -2, -1,  0,  1,  2,  3, 3, 3, 2, 1, 0, -2, -2,  3, 3, 2, 3],  # noqa: E241
-            'y': [0, 0, 0, 1, 2, 2,  2,  1,  0, -1, -2, -2, -2, -2, -1, 0, 1, 2, 3, 3,  2, -2, -2, 2, 3, 3],  # noqa: E241
-            'z': [0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 0, 0, 0, 0, 0,  0,  0,  0, 0, 0, 0]   # noqa: E241
-        }
-    )
-    max_digits = len(str(spiralsorted_expected.index.max()))
-    node_id = ["N_" + (max_digits - len(str(idx))) * '0' + str(idx)
-               for idx in spiralsorted_expected.index]
-    spiralsorted_expected.insert(0, "node_id", node_id)
-    spiralsorted_expected = spiralsorted_expected.astype(
-        {'x': "float", 'y': "float", 'z': "float"}
-    )
+class TestIo:
+    """io.py tests"""
 
-    # format
-    # ---------------------------
-    #    node_id    x    y    z
-    # 0     N_00  0.0  0.0  0.0
-    # 1     N_01  1.0  0.0  0.0
-    # ...
+    def test_output_file_path(self):
+        input_file_path_mock = "path/to/file.csv"
+        output_file_path_expected = "path/to/file_spiralsorted.csv"
+        output_file_path_expected_xlsx = "path/to/file_spiralsorted.xlsx"
+        assert output_file_path_expected == \
+            io.output_file_path(input_file_path_mock)
+        assert output_file_path_expected_xlsx == \
+            io.output_file_path(input_file_path_mock, output_format="xlsx")
 
-    #             19  18  24   25
-    #
-    #      20  6   5   4  17   23
-    #
-    #       7              3   16
-    #
-    #       8      0   1   2   15
-    #
-    #       9                  14
-    #
-    #      21 10  11  12  13   22
+    def test_animation_name(self):
+        input_file_path_mock = "path/to/file.csv"
+        ani_name = "path/to/file.mp4"
+        assert ani_name == io.animation_name(input_file_path_mock)
 
-    nodes_mock = spiralsorted_expected.copy()
-    # shuffle the DataFrame
-    nodes_mock = nodes_mock.sample(frac=1, random_state=9) \
-                           .reset_index(drop=True)
-    spiralsorted = core.spiralsort(nodes_mock,
-                                   spiralsorted_expected.loc[0, "node_id"])
-    assert_frame_equal(spiralsorted_expected,
-                       spiralsorted.iloc[:, [0, 1, 2, 3]])
+    def test_read_data_file(self):
+        data_path = os.path.join("examples", "data_examples",
+                                 "point_cloud_example.csv")
+        dataset = io.read_data_file(data_path)
+        assert isinstance(dataset, pd.DataFrame)
+        columns_expected = np.array(["node_id", 'x', 'y', 'z'])
+        np.array_equal(dataset.columns, columns_expected)
 
 
-# =============================================================================
-# io.py tests
-def test_output_file_path():
-    input_file_path_mock = "path/to/file.csv"
-    output_file_path_expected = "path/to/file_spiralsorted.csv"
-    output_file_path_expected_xlsx = "path/to/file_spiralsorted.xlsx"
-    assert output_file_path_expected == \
-        io.output_file_path(input_file_path_mock)
-    assert output_file_path_expected_xlsx == \
-        io.output_file_path(input_file_path_mock, output_format="xlsx")
+class TestUtils:
+    """utils.py tests"""
 
-def test_animation_name():
-    input_file_path_mock = "path/to/file.csv"
-    ani_name = "path/to/file.mp4"
-    assert ani_name == io.animation_name(input_file_path_mock)
+    def test_duplicated_ids(self):
+        nodes_mock_passing = pd.DataFrame({"node_id": ["N_00", "N_01", "N_02"]})
+        nodes_mock_failling = pd.DataFrame({"node_id": ["N_00", "N_01", "N_00"]})
+        with pytest.raises(Exception) as excifno:
+            utils.check_duplicated_ids(nodes_mock_failling)
+        assert utils.check_duplicated_ids(nodes_mock_passing) is True
 
-def test_read_data_file():
-    data_path = os.path.join("examples", "data_examples",
-                             "point_cloud_example.csv")
-    dataset = io.read_data_file(data_path)
-    assert isinstance(dataset, pd.DataFrame)
-    columns_expected = np.array(["node_id", 'x', 'y', 'z'])
-    np.array_equal(dataset.columns, columns_expected)
-
-
-# =============================================================================
-# utils.py tests
-def test_duplicated_ids():
-    nodes_mock_passing = pd.DataFrame({"node_id": ["N_00", "N_01", "N_02"]})
-    nodes_mock_failling = pd.DataFrame({"node_id": ["N_00", "N_01", "N_00"]})
-    with pytest.raises(Exception) as excifno:
-        utils.check_duplicated_ids(nodes_mock_failling)
-    assert utils.check_duplicated_ids(nodes_mock_passing) is True
-
-def test_calc_half_slice():
-    assert utils.calc_half_slice(slice(1, 6)) == 2
-    assert utils.calc_half_slice(slice(1, 7)) == 3
+    def test_calc_half_slice(self):
+        assert utils.calc_half_slice(slice(1, 6)) == 2
+        assert utils.calc_half_slice(slice(1, 7)) == 3
